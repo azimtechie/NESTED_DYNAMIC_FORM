0b43764c77f980596ab74fe1e77beb6a5fa31aec
ravi <rc@akkomplish.com>
2023-10-06 15:48:18 +0530

    'sendValueSomeFunc'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,7 +19,128 @@ export class AppComponent {
   constructor(private fb: FormBuilder) {
     this._createForm();
   }
+  jsonData = {
+    templateId: 11,
 
+    templateName: 'Ahmedabad Player Information',
+
+    description: 'Ahmedabad Player Information for training',
+
+    templateControls: [
+      {
+        controlId: 3,
+
+        label: 'Full Name',
+
+        controlValues: [
+          {
+            id: 0,
+
+            value: null,
+          },
+        ],
+
+        parentId: null,
+
+        isRequired: true,
+
+        sequence: 1,
+
+        templateControls: [],
+      },
+
+      {
+        controlId: 5,
+
+        label: 'Date of Birth',
+
+        controlValues: [
+          {
+            id: 0,
+
+            value: null,
+          },
+        ],
+
+        parentId: null,
+
+        isRequired: true,
+
+        sequence: 2,
+
+        templateControls: [],
+      },
+
+      {
+        controlId: 1,
+
+        label: 'Gender',
+
+        controlValues: [
+          {
+            id: 102,
+
+            value: 'Male',
+          },
+        ],
+
+        parentId: null,
+
+        isRequired: true,
+
+        sequence: 3,
+
+        templateControls: [],
+      },
+
+      {
+        controlId: 3,
+
+        label: 'Contact Number',
+
+        controlValues: [
+          {
+            id: 0,
+
+            value: null,
+          },
+        ],
+
+        parentId: null,
+
+        isRequired: true,
+
+        sequence: 4,
+
+        templateControls: [
+          {
+            controlId: 3,
+
+            label: 'Contact Number Secondary',
+
+            controlValues: [
+              {
+                id: 0,
+
+                value: null,
+              },
+            ],
+
+            parentId: 76,
+
+            isRequired: true,
+
+            sequence: 5,
+
+            templateControls: [],
+          },
+        ],
+      },
+    ],
+  };
+  sendValues(){
+    this._form.patchValue(this.jsonData)
+  }
   _addGroup() {
     this._groupsFormArray.push(
       this.fb.control({
