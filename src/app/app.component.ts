6a22abf0e6689ddbfcc093ca19c30494ae27a08e
abdulazim1512 <abdulazim1512@outlook.com>
2023-10-06 16:08:56 +0530

    'JsonFormating'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,125 +21,59 @@ export class AppComponent {
   }
   jsonData = {
     templateId: 11,
-
     templateName: 'Ahmedabad Player Information',
-
     description: 'Ahmedabad Player Information for training',
-
     templateControls: [
       {
         controlId: 3,
-
         label: 'Full Name',
-
-        controlValues: [
-          {
-            id: 0,
-
-            value: null,
-          },
-        ],
-
+        controlValues: [{ id: 0, value: null }],
         parentId: null,
-
-        isRequired: true,
-
+        isRequired: false,
         sequence: 1,
-
         templateControls: [],
       },
-
       {
         controlId: 5,
-
         label: 'Date of Birth',
-
-        controlValues: [
-          {
-            id: 0,
-
-            value: null,
-          },
-        ],
-
+        controlValues: [{ id: 0, value: null }],
         parentId: null,
-
         isRequired: true,
-
         sequence: 2,
-
         templateControls: [],
       },
-
       {
         controlId: 1,
-
         label: 'Gender',
-
-        controlValues: [
-          {
-            id: 102,
-
-            value: 'Male',
-          },
-        ],
-
+        controlValues: [{ id: 102, value: 'Male' }],
         parentId: null,
-
         isRequired: true,
-
         sequence: 3,
-
         templateControls: [],
       },
-
       {
         controlId: 3,
-
         label: 'Contact Number',
-
-        controlValues: [
-          {
-            id: 0,
-
-            value: null,
-          },
-        ],
-
+        controlValues: [{ id: 0, value: null }],
         parentId: null,
-
         isRequired: true,
-
         sequence: 4,
-
         templateControls: [
           {
             controlId: 3,
-
             label: 'Contact Number Secondary',
-
-            controlValues: [
-              {
-                id: 0,
-
-                value: null,
-              },
-            ],
-
+            controlValues: [{ id: 0, value: null }],
             parentId: 76,
-
             isRequired: true,
-
             sequence: 5,
-
             templateControls: [],
           },
         ],
       },
     ],
   };
-  sendValues(){
-    this._form.patchValue(this.jsonData)
+  sendValues() {
+    this._form.patchValue(this.jsonData);
   }
   _addGroup() {
     this._groupsFormArray.push(
